2ad8b83ef883e52c7f5f186a97d94e9c0fb86fd7
vavlxxx <vladimir.nefedov357@gmail.com>
2025-09-04 13:14:15 +0500

    auth: created users model + added type and owner for token

--- src/models/auth.py
+++ src/models/auth.py
@@ -1,10 +1,25 @@
 from datetime import datetime
 
-from sqlalchemy.orm import Mapped
+from sqlalchemy import ForeignKey, Text
+from sqlalchemy.dialects.postgresql import ENUM
+from sqlalchemy.orm import Mapped, mapped_column
 
 from src.models.base import Base
+from src.schemas.auth import TokenType
 
 
 class Token(Base):
+    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    type: Mapped[TokenType] = mapped_column(ENUM(TokenType))
     hashed_data: Mapped[str]
     expires_at: Mapped[datetime]
+
+
+class User(Base):
+    username: Mapped[str]
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    birth_date: Mapped[datetime]
+    hashed_password: Mapped[str]
+    email: Mapped[str]
+    bio: Mapped[str] = mapped_column(Text, nullable=True)
--- src/models/base.py
+++ src/models/base.py
@@ -1,11 +1,21 @@
 from datetime import datetime
 
-from sqlalchemy import Integer, func
+from sqlalchemy import Integer, MetaData, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+metadata_obj = MetaData(naming_convention=convention)
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+    metadata = metadata_obj
 
     id: Mapped[int] = mapped_column(
         Integer,
